5cea8173b9c6d4b690419583389e69ebfdc926be
man ofwar <jkthebeast69@gmail.com>
2025-11-14 14:24:25 +0000

    CAN I RUN THIS IN VISUAL STUDIO

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,7 +8,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components_ui_card';
+} from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
